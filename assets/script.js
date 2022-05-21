58daef060f704bc30b4a6e05761d234b0f8d5eb8
Jesus Islas <jesusislas96@gmail.com>
2022-05-21 19:58:15 -0400

    created question array in javascript

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+const quizQuestions = [
+    {
+        question: "Commonly used data types do not include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        question: "the condition of an if/else statement is enclosed within:",
+        choices: ["quotes", "curly brackets", "parentheses", "numbers"],
+        answer: "curly brackets"
+    },
+    {
+        question: "Arrays in Javascript can be used to store: ",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        question: "String values must be inclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        question: "A useful tool used during development and debugging which prints content to the debugger is: ",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    }
+
+]   
