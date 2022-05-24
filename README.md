46e15d40057aeff6348ad127854274d82a96270a
Jesus Islas <jesusislas96@gmail.com>
2022-05-24 16:44:55 -0400

    refactored javascript to allow choice buttons to refreshwith each loop. added screenshots and wrote project description in readme.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # 04-Quiz-Application
+
+This project was an attempt to make an application that creates a timed coding quiz.
+This application runs in the browswer and present the user with a timed quiz and their scores.
+
+This application begins with a landing page which explains the quiz and features a 'start quiz' button which then begins the 5 question quiz on a one minute timer. For each incorrect question, the user is docked 5 seconds from the timer. At the end of the five questions, a heading appears letting the user know that the game is over. 
+
+Link to deployed site: 
+https://jgislaszapata.github.io/04-Quiz-Application/
+
+Link to github repository:
+https://github.com/jgislaszapata/04-Quiz-Application
+
+
+screenshots of application can be found in the main root of this project. 
+
--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,20 @@
 // Initial declarations
-const landingPage = document.getElementById('landing-page');
-const startButton = document.getElementById('btn-start');
-const quizPage = document.getElementById('quiz-screen');
-const questionBody = document.getElementById('question-area');
-const choicesUl = document.getElementById('choices-ul');
-const answerFeedback = document.getElementById('user-feedback');
+var landingPage = document.getElementById('landing-page');
+var startButton = document.getElementById('btn-start');
+var quizPage = document.getElementById('quiz-screen');
+var questionBody = document.getElementById('question-area');
+var answerFeedback = document.getElementById('user-feedback');
+
+//Timer declarations 
+const timeRemaining = document.getElementById("time-remaining");
+var secondsLeft = 60;
+var timerFloor = 0;
+var timePenalty = 5;
 
 // This array contains each question, their responses, and the correct answer
-let userScore = 0;
-let questionIndex = 0;
-const quizQuestions = [
+var userScore = 0;
+var arrayIndex = 0;
+var quizQuestions = [
     {
         question: "Commonly used data types do not include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
@@ -37,39 +42,80 @@ const quizQuestions = [
     }
 ]   
 
-// The following three lines will erase the landing page
-startButton.addEventListener("click", clearLandingPage);
-function clearLandingPage() {
+// 'Start quiz' starts the countdown
+startButton.addEventListener("click", function() {
+    console.log('Start button clicked, begin timer');
+    if (timerFloor === 0) {
+        timerFloor = setInterval(function() {
+            secondsLeft--;
+            timeRemaining.textContent = "Time remaining:" + secondsLeft;
+                if (secondsLeft <= 0) {
+                    clearInterval(timeCheck);
+                    gameOver();
+                }
+        }, 1000);
+    } showQuiz(arrayIndex);
+});
+
+
+var choicesUl = document.createElement("ul");
+choicesUl.setAttribute("id", "choices-ul")
+
+function showQuiz(arrayIndex) {
     landingPage.style.display = "none";
-    console.log(`'Start Quiz' button clicked.`)
-    showQuiz();
-
-}
-
-// Displays initial question and set of answers 
-function showQuiz() {
-   for (i = 0; i <quizQuestions.length -1; i++) {
-    let currentQuestionBody = quizQuestions[questionIndex].question;
-    questionBody.textContent = currentQuestionBody;
-    let currentChoiceList = quizQuestions[questionIndex].choices[i];
-    let choicesButtons = document.createElement("li");
-    choicesButtons.setAttribute("type", "button");
-    choicesButtons.setAttribute("class", "btn btn-primary");
-    choicesButtons.setAttribute("style", "color: white; background-color: blue; width: 140px; height: 90px; padding-top: 12px; margin: 18px; border-radius: 10px; ");
-    choicesUl.appendChild(choicesButtons);
-    choicesButtons.textContent = currentChoiceList;
-    choicesButtons.addEventListener("click", function (nextQuestion){
-        questionIndex++;
-    });
-   
+    questionBody.innerHTML = "";
+    choicesUl.innerHTML = "";
+    for (var i = 0; i < quizQuestions.length; i++) {
+        var currentQuestionBody = quizQuestions[arrayIndex].question;
+        questionBody.textContent = currentQuestionBody;
+        var choiceItems = quizQuestions[arrayIndex].choices;
     }
 
-   
-   
+    choiceItems.forEach(function (listText) {
+        var choicesList = document.createElement("li");
+        choicesList.setAttribute("style", "color: white; background-color: green; width: 20%; height: 100px; font-size: 1.5em; text-align: center; padding-top: 20px; margin: 20px; border-radius: 8px;")
+        choicesList.textContent = listText;
+        quizPage.appendChild(choicesUl);
+        choicesUl.appendChild(choicesList);
+        choicesList.addEventListener("click", (answerEval));
+    })
 }
 
+function answerEval(event) {
+    var index = event.target;
 
+    if (index.matches("li")) {
 
+        var feedBackText = document.createElement("p");
+        feedBackText.setAttribute("id", "feedBackText");
+        if (index.textContent == quizQuestions[arrayIndex].answer) {
+            feedBackText.textContent = "Correct! Great job!";
+        } else {
+            secondsLeft = secondsLeft - timePenalty;
+            feedBackText.textContent = "Wrong! 5 seconds deducted from time";
+        }
+
+    arrayIndex++;
+
+    if (arrayIndex >= quizQuestions.length) {
+        gameOver();
+        feedBackText.textContent = `Game over! your final score is ${userScore}`;
+    } else {
+        showQuiz(arrayIndex);
+    }
+    }
+}
+
+function gameOver() {
+    questionBody.innerHTML = "";
+    timeRemaining.style.display = "none";
+    choicesUl.style.display = "none";
+
+    var headingComplete = document.createElement("h1");
+    headingComplete.textContent = `Game Over`;
+
+    questionBody.appendChild(headingComplete);
+}
 
 
 
